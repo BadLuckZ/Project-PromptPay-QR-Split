7eb1016aa0f423c3096b759e025a804a162b68af
BadLuckZ <thanagorn8802@gmail.com>
2026-06-20 23:57:41 +0700

    chore: setup css

--- app/page.tsx
+++ app/page.tsx
@@ -4,5 +4,5 @@ export default async function Home() {
   const { data, error } = await SUPABASE.from("bills").select("*");
   console.log("data:", data, "error:", error);
 
-  return <h1 className="text-2xl font-bold">Hello World!</h1>;
+  return <h1 className="text-2xl font-bold text-primary">Hello World!</h1>;
 }
